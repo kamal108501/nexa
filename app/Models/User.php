f6adc231f98eb7db5063dab9f15dcd99ff2615ea
kamal10851 <kamal1085@gmail.com>
2025-12-28 22:30:24 +0530

    change in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true; // allow the only user
+    }
 
     /**
      * The attributes that are mass assignable.
